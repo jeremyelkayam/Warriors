002ed79b33a60eddbe1a00d15fca64d7a2ec71bd
Jeremy Elkayam <jeremyelkayam@gmail.com>
2020-09-21 23:33:20 -0400

    fixed buggy warrior but not control bug

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -8,7 +8,7 @@
 
 void Enemy::point_at_player(float player_x, float player_y){
   float delta_x = player_x - get_xcor();
-  float delta_y = get_ycor() - player_y;
+  float delta_y = player_y - get_ycor() ;
 
   angle = atan2(delta_y,delta_x);
 }
